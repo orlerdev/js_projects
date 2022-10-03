974ed347074280443cc963c79be96d64404f190c
Jared Orler <orlerjared@gmail.com>
2022-10-03 15:43:21 -0700

    Initial commit

---
+++ React2022/feedbackapp/src/App.js
@@ -0,0 +1,65 @@
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+
+function App() {
+	const [feedback, setFeedback] = useState(FeedbackData)
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
+
+	const deleteFeedback = (id) => {
+		if (window.confirm('Are you sure you want to delete')) {
+			setFeedback(feedback.filter((item) => item.id !== id))
+		}
+	}
+
+	return (
+		<Router>
+			<Header />
+			<div className='container'>
+				<FeedbackForm handleAdd={addFeedback} />
+				<FeedbackStats feedback={feedback} />
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+
+				<Route path='/about' element={AboutPage} />
+
+				<AboutIconLink />
+			</div>
+		</Router>
+	)
+}
+
+export default App
+
+// return (
+// 	<Router>
+// 		<Header />
+// 		<div className='container'>
+// 			<FeedbackForm handleAdd={addFeedback} />
+// 			<FeedbackStats feedback={feedback} />
+// 			<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+
+// 			<Route path='/about' component={AboutPage} />
+// 		</div>
+// 	</Router>
+// )
+
+// return (
+// 	<Router>
+// 		<Header />
+// 		<div className='container'>
+// 			<FeedbackForm handleAdd={addFeedback} />
+// 			<FeedbackStats feedback={feedback} />
+// 			<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+// 		</div>
+// 	</Router>
+// )
---
+++ React2022/feedbackapp/src/data/FeedbackData.jsx
@@ -0,0 +1,19 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Anything works',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'Anything works',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'Anything works',
+	},
+]
+
+export default FeedbackData
